b5fc7a5a51225216a8f32982712bd90db62f9fd6
Gpeder <gtzyy008@gmail.com>
2024-12-05 14:42:36 -0300

    javascript pronto

---
+++ src/scripts/script.js
@@ -0,0 +1,59 @@
+const drops = document.querySelectorAll(".arrow");
+
+drops.forEach(dropdown => {
+    dropdown.addEventListener("click", () => {
+        const body = dropdown.parentElement.nextElementSibling; 
+        dropdown.classList.toggle("active");
+        body.classList.toggle("active");
+
+        if (dropdown.classList.contains("active")) {
+            dropdown.style.transform = "rotate(180deg)";
+        } else {
+            dropdown.style.transform = "rotate(0deg)";
+        }
+    });
+});
+
+const img = document.querySelector('img');
+
+function callback(e){
+    console.log(e);
+}
+
+img.addEventListener('click', callback);
+
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabcontent = document.querySelectorAll('.js-tabcontent section');
+tabcontent[0].classList.add('active');
+
+if(tabmenu.length && tabcontent.length){
+    function activeTab(index){
+        tabcontent.forEach((section)=>{
+            section.classList.remove('active');
+        });
+        tabcontent[index].classList.add('active');
+    }
+    
+    tabmenu.forEach((itemMenu, index)=>{
+        itemMenu.addEventListener('click', function(){
+            activeTab(index);
+        })
+    })
+}
+
+const linksinternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+
+scrolltosection = (event) => {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+}
+
+linksinternos.forEach((link) => {
+    link.addEventListener('click', scrolltosection);
+});
